f0d0758542eb5d185de9606eef4c3d6a5b1a23c5
Sammysk12 <samarthkalshetti43@gmail.com>
2024-09-03 21:56:01 +0530

    Developing a fully functional and responsive website in React JS and added some basic components in it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Article from './Components/article/Article'
+import Brand from './Components/brand/Brand'
+import CTA from './Components/cta/CTA'
+import Feature from './Components/feature/Feature'
+import Navbar from './Components/navbar/Navbar'
 
-function App() {
+import Blog from './Containers/blog/Blog'
+import Features from './Containers/features/Features'
+import Footer from './Containers/footer/Footer'
+import Header from './Containers/header/Header'
+import Possibility from './Containers/possibility/Possibility'
+import WhatGPT3 from './Containers/whatGPT3/WhatGPT3'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar/>
+          <Header/>
+        </div>
+        
+        <Brand/>
+        <WhatGPT3/>
+        <Features/>
+        <Possibility/>
+        <CTA/>
+        <Blog/>
+        <Footer/>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/brand/Brand.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './brand.css';
+
+const Brand = () => {
+  return (
+    <div>Article</div>
+  )
+}
+
+export default Brand
